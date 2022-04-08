d82df7dade39b8bc9685ec7999456dc45f854f24
Yonghwan SO <scinix@gmail.com>
2022-04-08 21:28:43 +0900

    test: internal/common/context

--- exporter.go
+++
@@ -1,6 +0,0 @@
-package bogo
-
-// PingExporter that needs to be implemented to be a PingMessage exporter
-type PingExproter interface {
-	Initialize(in chan PingMessage, wait chan int)
-}
---
+++ internal/common/context_test.go
@@ -0,0 +1,69 @@
+package common
+
+import (
+	"context"
+	"sync"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestContext(t *testing.T) {
+	r := require.New(t)
+
+	opt := Options{}
+	c, ccf := NewDefaultContext(&opt)
+	r.NotNil(c)
+	r.NotNil(ccf)
+
+	r.IsType((*DefaultContext)(nil), c)
+	r.Implements((*Context)(nil), c)
+	r.Implements((*context.Context)(nil), c)
+	r.IsType((context.CancelFunc)(nil), ccf)
+}
+
+func TestContextCancel(t *testing.T) {
+	r := require.New(t)
+
+	opt := Options{}
+	c, ccf := NewDefaultContext(&opt)
+	r.NotNil(c)
+	r.NotNil(ccf)
+
+	r.Nil(c.Err())
+	c.Cancel()
+	r.NotNil(c.Err())
+}
+
+func TestContextWG(t *testing.T) {
+	r := require.New(t)
+
+	opt := Options{}
+	c, ccf := NewDefaultContext(&opt)
+	r.NotNil(c)
+	r.NotNil(ccf)
+
+	r.IsType((*sync.WaitGroup)(nil), c.WG())
+}
+
+func TestContextLogger(t *testing.T) {
+	r := require.New(t)
+
+	opt := Options{}
+	c, ccf := NewDefaultContext(&opt)
+	r.NotNil(c)
+	r.NotNil(ccf)
+
+	r.Implements((*Logger)(nil), c.Logger())
+}
+
+func TestContextMeta(t *testing.T) {
+	r := require.New(t)
+
+	opt := Options{}
+	c, ccf := NewDefaultContext(&opt)
+	r.NotNil(c)
+	r.NotNil(ccf)
+
+	r.IsType((MetaClient)(nil), c.Meta())
+}
